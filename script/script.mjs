5f0a4ef3e332904b23f7191a173e57df38912af2
JinZ <bbcy777@gmail.com>
2024-05-21 13:56:52 -0700

    need to learn localStorage

--- script/script.mjs
+++ script/script.mjs
@@ -1,3 +1,17 @@
+//validate username
+const user = document.getElementById(`username`);
+const form = document.getElementById(`user`);
+
+form.addEventListener('submit',validateUsername)
+
+function validateUsername(){
+    const usernameValue = user.value.trim();
+    if(!usernameValue.match(/^[a-zA-Z]+$/)){
+        user.focus();
+        alert(`Name cannot contain any special characters or numbers`)
+        return false;
+    }
+}
 //when user submit name on nav bar, show the cart div
 
 //when user add an item in cart, move the h5 element to cart
